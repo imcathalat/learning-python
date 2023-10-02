fd4a2584d733cb27b42eb86390c155f2603e45b5
Isadora <imcathalat@gmail.com>
2023-10-02 17:24:20 -0300

    changes

--- temperature.py
+++ temperature.py
@@ -1,19 +1,19 @@
-fahrenheit = float(input("Enter with a temperature in Fahrenheit: "))
+fahrenheit = float(input("Enter a temperature in degrees F: "))
 
 def convert_fah_to_cel(f):
     celsius = (f- 32) * (5/9)
     return celsius
 
 celsius_convertidos = convert_fah_to_cel(fahrenheit)
-print(f"{fahrenheit} graus fahrenheit são {celsius_convertidos} graus celsius")
+print(f"{fahrenheit} degrees F = {celsius_convertidos} degrees C")
 
-celsius = float(input("Enter with a temperature in Celsius: "))
+celsius = float(input("\nEnter a temperature in degrees C: "))
 
 def convert_cel_to_fah(c):
-    """converte graus celsius para fahrenheit"""
+    """converte graus celsius para fahrenheit""" #docstring
     fahrenheit = c * (9/5) + 32
     return fahrenheit
 
 fah_convertidos = convert_cel_to_fah(celsius)
 
-print(f"{celsius} graus celsius são {fah_convertidos} graus fahrenheit")
+print(f"{celsius} degrees C = {fah_convertidos} degrees F")
